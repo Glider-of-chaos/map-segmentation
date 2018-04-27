8d44ac0aefe9694e69705d1cbf065e56d9b6f52c
Glider <g4regtrash@gmail.com>
2018-04-27 22:24:20 +0300

    indent fix + readme 1st draft

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # map-segmentation
+## Purpose:
+The goal is to parse the lines csv file to map line ids to map section ids. Map sections are roughly rectangular and are based on just adding 0.01 incriments to latitude and longitude. Relevant sections are provided in the csv file as well
+
+**lines csv file format:**
+"line_id";"start_latitude";"start_longitude";"end_latitude";"end_longitude"
+**sections csv file format:**
+"section_id","max_latitude","min_latitude","min_longitude","max_longitude"
+
+## The plan:
+The plan is to create a GeoLine class with the following contents
+- ray_intersection_point - to find the point where those line would intersect if they did not end at the start and end points
+- includes_point - checks if the point belongs to this line
+- intersects - to check if lines actually intersect
+- find_sections - actual map segmentation
--- map_segmentation.py
+++ map_segmentation.py
@@ -3,245 +3,245 @@ import math
 
 class GeoLine:
 
-	def __init__(self, coords_one, coords_two):
-		if coords_one[0] < coords_two[0]:
-			self.start = coords_one
-			self.end = coords_two
-		elif coords_one[0] > coords_two[0]:
-			self.start = coords_two
-			self.end = coords_one
-		else:
-			if coords_one[1] < coords_two[1]:
-				self.start = coords_one
-				self.end = coords_two
-			else:
-				self.start = coords_two
-				self.end = coords_one
-				
-	def __str__(self):
-		return (self.start, self.end).__str__()
-				
-	def ray_intersection_point(self, line):
-		line1_lat_start = self.start[0]
-		line1_lon_start = self.start[1]
+    def __init__(self, coords_one, coords_two):
+        if coords_one[0] < coords_two[0]:
+            self.start = coords_one
+            self.end = coords_two
+        elif coords_one[0] > coords_two[0]:
+            self.start = coords_two
+            self.end = coords_one
+        else:
+            if coords_one[1] < coords_two[1]:
+                self.start = coords_one
+                self.end = coords_two
+            else:
+                self.start = coords_two
+                self.end = coords_one
+                
+    def __str__(self):
+        return (self.start, self.end).__str__()
+                
+    def ray_intersection_point(self, line):
+        line1_lat_start = self.start[0]
+        line1_lon_start = self.start[1]
 
-		line1_lat_end = self.end[0]
-		line1_lon_end = self.end[1]
+        line1_lat_end = self.end[0]
+        line1_lon_end = self.end[1]
 
-		line2_lat_start = line.start[0]
-		line2_lon_start = line.start[1]
+        line2_lat_start = line.start[0]
+        line2_lon_start = line.start[1]
 
-		line2_lat_end = line.end[0]
-		line2_lon_end = line.end[1]
-		
-		line1_lon_const = False
-		line2_lon_const = False
-		slope1 = float('nan')
-		slope2 = float('nan')
-		
-		try:
-			slope1 = self.slope()
-		except ZeroDivisionError:
-			line1_lon_const = True
-			
-		try:
-			slope2 = line.slope()
-		except ZeroDivisionError: 
-			line2_lon_const = True
-			
-		if (line1_lon_const and line2_lon_const) or (slope1 == slope2):
-			if line1_lat_start == line2_lat_start:
-				return self
-			else:
-				return float('nan')
-		
-		if line1_lon_const:
-			lon = line1_lon_start
-			#try:
-			lat = line.lat(lon)
-			#except ZeroDivisionError:
-				#lon = line2_lon_start
-			return (lat, lon)
-			
-		if line2_lon_const:
-			lon = line2_lat_start
-			#try:
-			lat = self.lat(lon)
-			#except ZeroDivisionError:
-			#	lon = line1_lon_start
-			return (lat, lon)
-			
-#	lat= line1_lat_start + (lon - line1_lon_start) * slope1
-#	lat= line2_lat_start + (lon - line2_lon_start) * slope2
-		
-#		lon = (line2_lat_start - line1_lat_start) / \
-#			( (line1_lon_end - line1_lon_start)/(line1_lat_end - line1_lat_start) -\
-#			(line2_lon_end - line2_lon_start)/(line2_lat_end - line2_lat_start) )
+        line2_lat_end = line.end[0]
+        line2_lon_end = line.end[1]
+        
+        line1_lon_const = False
+        line2_lon_const = False
+        slope1 = float('nan')
+        slope2 = float('nan')
+        
+        try:
+            slope1 = self.slope()
+        except ZeroDivisionError:
+            line1_lon_const = True
+            
+        try:
+            slope2 = line.slope()
+        except ZeroDivisionError: 
+            line2_lon_const = True
+            
+        if (line1_lon_const and line2_lon_const) or (slope1 == slope2):
+            if line1_lat_start == line2_lat_start:
+                return self
+            else:
+                return float('nan')
+        
+        if line1_lon_const:
+            lon = line1_lon_start
+            #try:
+            lat = line.lat(lon)
+            #except ZeroDivisionError:
+                #lon = line2_lon_start
+            return (lat, lon)
+            
+        if line2_lon_const:
+            lon = line2_lat_start
+            #try:
+            lat = self.lat(lon)
+            #except ZeroDivisionError:
+            #    lon = line1_lon_start
+            return (lat, lon)
+            
+#    lat= line1_lat_start + (lon - line1_lon_start) * slope1
+#    lat= line2_lat_start + (lon - line2_lon_start) * slope2
+        
+#        lon = (line2_lat_start - line1_lat_start) / \
+#            ( (line1_lon_end - line1_lon_start)/(line1_lat_end - line1_lat_start) -\
+#            (line2_lon_end - line2_lon_start)/(line2_lat_end - line2_lat_start) )
 
-#		line1_lat_start + lon*slope1 - line1_lon_start*slope1 = line2_lat_start + lon*slope2 - line2_lon_start*slope2
-#		lon(slope1-slope2) = line2_lat_start -line1_lat_start - line2_lon_start*slope2 + line1_lon_start*slope1
-		
-		lon = (line2_lat_start -line1_lat_start - line2_lon_start*slope2 + line1_lon_start*slope1)/(slope1-slope2)
-			
-		lat= line1_lat_start + (lon - line1_lon_start) * slope1
-		
-		return (lat, lon)
-		
-	def slope(self):
-		line1_lat_start = self.start[0]
-		line1_lon_start = self.start[1]
-		
-		line1_lat_end = self.end[0]
-		line1_lon_end = self.end[1]
+#        line1_lat_start + lon*slope1 - line1_lon_start*slope1 = line2_lat_start + lon*slope2 - line2_lon_start*slope2
+#        lon(slope1-slope2) = line2_lat_start -line1_lat_start - line2_lon_start*slope2 + line1_lon_start*slope1
+        
+        lon = (line2_lat_start -line1_lat_start - line2_lon_start*slope2 + line1_lon_start*slope1)/(slope1-slope2)
+            
+        lat= line1_lat_start + (lon - line1_lon_start) * slope1
+        
+        return (lat, lon)
+        
+    def slope(self):
+        line1_lat_start = self.start[0]
+        line1_lon_start = self.start[1]
+        
+        line1_lat_end = self.end[0]
+        line1_lon_end = self.end[1]
 
-		# may be should've done try/except? does not seem to do much
-		#try:
-		slope = (line1_lat_end - line1_lat_start)/(line1_lon_end - line1_lon_start)
-		#except ZeroDivisionError:
+        # may be should've done try/except? does not seem to do much
+        #try:
+        slope = (line1_lat_end - line1_lat_start)/(line1_lon_end - line1_lon_start)
+        #except ZeroDivisionError:
 
-		return slope
-		
-	def includes_point(self, point):
-		
-		point_lat = point[0]
-		point_lon = point[1]
-		
-		line1_lat_start = self.start[0]
-		line1_lon_start = self.start[1]
+        return slope
+        
+    def includes_point(self, point):
+        
+        point_lat = point[0]
+        point_lon = point[1]
+        
+        line1_lat_start = self.start[0]
+        line1_lon_start = self.start[1]
 
-		line1_lat_end = self.end[0]
-		line1_lon_end = self.end[1]
-		
-		if  point_lat >= self.start[0] and point_lat <= self.end[0] and point_lon >= self.start[1] and point_lon <= self.end[1]:
-			if point_lat == line1_lat_start + point_lon * (line1_lon_end - line1_lon_start)/(line1_lat_end - line1_lat_start):
-				return True
-		print ( line1_lat_start + point_lon * (line1_lon_end - line1_lon_start)/(line1_lat_end - line1_lat_start) )
-		return False
-		
-	def lat(self,lon):
-		line1_lat_start = self.start[0]
-		lat = self.start[0] + (lon - self.start[1])*self.slope()
-		return lat
-		
-	def intersects(self, line):
-		line1_lat_start = self.start[0]
-		line1_lon_start = self.start[1]
+        line1_lat_end = self.end[0]
+        line1_lon_end = self.end[1]
+        
+        if  point_lat >= self.start[0] and point_lat <= self.end[0] and point_lon >= self.start[1] and point_lon <= self.end[1]:
+            if point_lat == line1_lat_start + point_lon * (line1_lon_end - line1_lon_start)/(line1_lat_end - line1_lat_start):
+                return True
+        print ( line1_lat_start + point_lon * (line1_lon_end - line1_lon_start)/(line1_lat_end - line1_lat_start) )
+        return False
+        
+    def lat(self,lon):
+        line1_lat_start = self.start[0]
+        lat = self.start[0] + (lon - self.start[1])*self.slope()
+        return lat
+        
+    def intersects(self, line):
+        line1_lat_start = self.start[0]
+        line1_lon_start = self.start[1]
 
-		line1_lat_end = self.end[0]
-		line1_lon_end = self.end[1]
+        line1_lat_end = self.end[0]
+        line1_lon_end = self.end[1]
 
-		line2_lat_start = line.start[0]
-		line2_lon_start = line.start[1]
+        line2_lat_start = line.start[0]
+        line2_lon_start = line.start[1]
 
-		line2_lat_end = line.end[0]
-		line2_lon_end = line.end[1]
-		
-		line1_lon_const = False
-		line2_lon_const = False
-		slope1 = float('nan')
-		slope2 = float('nan')
-		
-		try:
-			slope1 = self.slope()
-		except ZeroDivisionError:
-			line1_lon_const = True
-			
-		try:
-			slope2 = line.slope()
-		except ZeroDivisionError: 
-			line2_lon_const = True
-			
-		if (line1_lon_const and line2_lon_const) or (slope1 == slope2):
-			if line1_lat_start == line2_lat_start:
-				return true
-			else:
-				return false
-		
-		elif line1_lon_const:
-			lon = line1_lon_start
-			#try:
-			lat = line.lat(lon)
-			#except ZeroDivisionError:
-				#lon = line2_lon_start
-			return (line1_lat_start <= lat and line1_lat_end >= lat)
-			
-		elif line2_lon_const:
-			lon = line2_lat_start
-			#try:
-			lat = self.lat(lon)
-			#except ZeroDivisionError:
-			#	lon = line1_lon_start
-			return (line2_lat_start <= lat and line2_lat_end >= lat)
-			
-		else:
-			lon = (line2_lat_start -line1_lat_start - line2_lon_start*slope2 + line1_lon_start*slope1)/(slope1-slope2)
-				
-			lat= line1_lat_start + (lon - line1_lon_start) * slope1
-		
-		return (line2_lat_start <= lat and line2_lat_end >= lat and line1_lat_start <= lat and line1_lat_end >= lat)
-		
-			
-	def find_sections(self):
-		lat_start = self.start[0]
-		lon_start = self.start[1]
-		lat_end = self.end[0]
-		lon_end = self.end[1]
-		
-		rounded_lat_start = math.floor(self.start[0]*100)/100
-		rounded_lon_start = math.floor(self.start[1]*100)/100
-		rounded_lat_end = math.floor(self.end[0]*100)/100
-		rounded_lon_end = math.floor(self.end[1]*100)/100
-		
-		sections = [(round(rounded_lat_start, 2), round(rounded_lon_start, 2))]
-		
-		if rounded_lat_start == rounded_lat_end and rounded_lon_start == rounded_lon_end:
-			return sections
-			
-		if rounded_lat_start != rounded_lat_end:
-			crossed_lats = round((rounded_lat_end - rounded_lat_start)*100)
-			crossed_lons = round((rounded_lon_end - rounded_lon_start)*100)
-			for i in range(0, crossed_lats + 1):
-				for j in range(0, crossed_lons + 1):
-					section_node = (round(rounded_lat_start + 0.01*i, 2), round(rounded_lon_start + 0.01*j, 2))
-					if (section_node) not in sections:
-						crossing_lat = GeoLine((rounded_lat_start + 0.01*i, rounded_lon_start + 0.01*j),
-						(rounded_lat_start + 0.01*(i+1), rounded_lon_start + 0.01*j))
-						crossing_lon = GeoLine((rounded_lat_start + 0.01*i, rounded_lon_start + 0.01*j),
-						(rounded_lat_start + 0.01*i, rounded_lon_start + 0.01*(j + 1)))
-						
-						lat_crossed = self.intersects(crossing_lat)
-						lon_crossed = self.intersects(crossing_lon)
-						
-						print("section: {0}:".format(section_node))
-						print("lat line: {0}, \tcrossed: {1}".format(crossing_lat, lat_crossed))
-						print("lon line: {0}, \tcrossed: {1}".format(crossing_lon, lon_crossed))
-						
-						if lat_crossed or lon_crossed:
-							sections.append(section_node)
-							
-		return sections
-		
+        line2_lat_end = line.end[0]
+        line2_lon_end = line.end[1]
+        
+        line1_lon_const = False
+        line2_lon_const = False
+        slope1 = float('nan')
+        slope2 = float('nan')
+        
+        try:
+            slope1 = self.slope()
+        except ZeroDivisionError:
+            line1_lon_const = True
+            
+        try:
+            slope2 = line.slope()
+        except ZeroDivisionError: 
+            line2_lon_const = True
+            
+        if (line1_lon_const and line2_lon_const) or (slope1 == slope2):
+            if line1_lat_start == line2_lat_start:
+                return true
+            else:
+                return false
+        
+        elif line1_lon_const:
+            lon = line1_lon_start
+            #try:
+            lat = line.lat(lon)
+            #except ZeroDivisionError:
+                #lon = line2_lon_start
+            return (line1_lat_start <= lat and line1_lat_end >= lat)
+            
+        elif line2_lon_const:
+            lon = line2_lat_start
+            #try:
+            lat = self.lat(lon)
+            #except ZeroDivisionError:
+            #    lon = line1_lon_start
+            return (line2_lat_start <= lat and line2_lat_end >= lat)
+            
+        else:
+            lon = (line2_lat_start -line1_lat_start - line2_lon_start*slope2 + line1_lon_start*slope1)/(slope1-slope2)
+                
+            lat= line1_lat_start + (lon - line1_lon_start) * slope1
+        
+        return (line2_lat_start <= lat and line2_lat_end >= lat and line1_lat_start <= lat and line1_lat_end >= lat)
+        
+            
+    def find_sections(self):
+        lat_start = self.start[0]
+        lon_start = self.start[1]
+        lat_end = self.end[0]
+        lon_end = self.end[1]
+        
+        rounded_lat_start = math.floor(self.start[0]*100)/100
+        rounded_lon_start = math.floor(self.start[1]*100)/100
+        rounded_lat_end = math.floor(self.end[0]*100)/100
+        rounded_lon_end = math.floor(self.end[1]*100)/100
+        
+        sections = [(round(rounded_lat_start, 2), round(rounded_lon_start, 2))]
+        
+        if rounded_lat_start == rounded_lat_end and rounded_lon_start == rounded_lon_end:
+            return sections
+            
+        if rounded_lat_start != rounded_lat_end:
+            crossed_lats = round((rounded_lat_end - rounded_lat_start)*100)
+            crossed_lons = round((rounded_lon_end - rounded_lon_start)*100)
+            for i in range(0, crossed_lats + 1):
+                for j in range(0, crossed_lons + 1):
+                    section_node = (round(rounded_lat_start + 0.01*i, 2), round(rounded_lon_start + 0.01*j, 2))
+                    if (section_node) not in sections:
+                        crossing_lat = GeoLine((rounded_lat_start + 0.01*i, rounded_lon_start + 0.01*j),
+                        (rounded_lat_start + 0.01*(i+1), rounded_lon_start + 0.01*j))
+                        crossing_lon = GeoLine((rounded_lat_start + 0.01*i, rounded_lon_start + 0.01*j),
+                        (rounded_lat_start + 0.01*i, rounded_lon_start + 0.01*(j + 1)))
+                        
+                        lat_crossed = self.intersects(crossing_lat)
+                        lon_crossed = self.intersects(crossing_lon)
+                        
+                        print("section: {0}:".format(section_node))
+                        print("lat line: {0}, \tcrossed: {1}".format(crossing_lat, lat_crossed))
+                        print("lon line: {0}, \tcrossed: {1}".format(crossing_lon, lon_crossed))
+                        
+                        if lat_crossed or lon_crossed:
+                            sections.append(section_node)
+                            
+        return sections
+        
 
 
-			
-#	lat= line1_lat_start + (lon - line1_lon_start) * slope1
-#	lat= line2_lat_start + (lon - line2_lon_start) * slope2
-		
-#		lon = (line2_lat_start - line1_lat_start) / \
-#			( (line1_lon_end - line1_lon_start)/(line1_lat_end - line1_lat_start) -\
-#			(line2_lon_end - line2_lon_start)/(line2_lat_end - line2_lat_start) )
+            
+#    lat= line1_lat_start + (lon - line1_lon_start) * slope1
+#    lat= line2_lat_start + (lon - line2_lon_start) * slope2
+        
+#        lon = (line2_lat_start - line1_lat_start) / \
+#            ( (line1_lon_end - line1_lon_start)/(line1_lat_end - line1_lat_start) -\
+#            (line2_lon_end - line2_lon_start)/(line2_lat_end - line2_lat_start) )
 
-#		line1_lat_start + lon*slope1 - line1_lon_start*slope1 = line2_lat_start + lon*slope2 - line2_lon_start*slope2
-#		lon(slope1-slope2) = line2_lat_start -line1_lat_start - line2_lon_start*slope2 + line1_lon_start*slope1
-		
-		
+#        line1_lat_start + lon*slope1 - line1_lon_start*slope1 = line2_lat_start + lon*slope2 - line2_lon_start*slope2
+#        lon(slope1-slope2) = line2_lat_start -line1_lat_start - line2_lon_start*slope2 + line1_lon_start*slope1
+        
+        
 sections_dict = {}
 with open("E:\Downloads\Programming\map\sections.csv", newline='') as csvfile:
-	csvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
-	for row in csvreader:
-		key = '-'.join((row[2], row[3]))
-		sections_dict[key] = row[0]
-		
+    csvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    for row in csvreader:
+        key = '-'.join((row[2], row[3]))
+        sections_dict[key] = row[0]
+        
 with open("E:\Downloads\Programming\map\dp_view_01.csv", newline='') as csvfile:
-	csvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
-	for row in csvreader:
+    csvreader = csv.reader(csvfile, delimiter=',', quotechar='"')
+    for row in csvreader:
